4ac6aca5fb4cf10fcdd44ba840b5b072df84b6e4
tohird03 <tohirjondoniyorov@gmail.com>
2026-01-08 08:32:42 +0500

    FIx app

--- src/modules/Layout/CloseDay/CloseDay.tsx
+++ src/modules/Layout/CloseDay/CloseDay.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import {observer} from 'mobx-react';
-import {LogoutOutlined} from '@ant-design/icons';
-import {Button, Modal, Typography} from 'antd';
+import React, { useState } from 'react';
+import { observer } from 'mobx-react';
+import { LogoutOutlined } from '@ant-design/icons';
+import { Button, Modal, Typography } from 'antd';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { authApi } from '@/api';
 import { addNotification } from '@/utils';
@@ -36,16 +36,18 @@ export const CloseDay: React.FC = observer(() => {
 
   return (
     <div>
-      <span onClick={showModal} className="logout">
-        <Button style={{width: '100%'}} type="primary" danger>Kunni yopish</Button>
-      </span>
+      {authStore?.staffInfo?.phone === '998981157574' &&
+        <span onClick={showModal} className="logout">
+          <Button style={{ width: '100%' }} type="primary" danger>Kunni yopish</Button>
+        </span>
+      }
       <Modal
         title={'Rostdan ham bugungi kunni yopmoqchimisiz?'}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
         okText="Yopish"
-        okButtonProps={{style: {backgroundColor: 'red'}}}
+        okButtonProps={{ style: { backgroundColor: 'red' } }}
         cancelText="Bekor qilish"
       />
     </div>
